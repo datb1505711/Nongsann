5da01ce7222108511b11c3980f7d1ac65b061388
phamhongson <sonb1505797@student.ctu.edu.vn>
2019-11-10 15:47:18 +0700

    update scroolview dang nhap bg dag nhap

--- NongSan/app/src/main/java/com/example/nongsan/DangNhapActivity.java
+++ NongSan/app/src/main/java/com/example/nongsan/DangNhapActivity.java
@@ -30,7 +30,6 @@ public class DangNhapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
         addControls();
-
         login();
 
 
